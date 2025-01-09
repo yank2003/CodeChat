a3a19cbabc6b2d706785857612f820f5b2212e9e
yank2003 <mayanklap23@gmail.com>
2025-01-09 14:00:16 +0530

    userauth

--- Frontend/src/auth/Userauth.jsx
+++ Frontend/src/auth/Userauth.jsx
@@ -1,21 +1,21 @@
-import { useNavigate } from "react-router-dom";
-import { UserContext } from "../context/user.context";
-import { useContext, useEffect, useState } from "react";
-const Userauth = ({ children }) => {
-  const navigate = useNavigate();
-  const { user } = useContext(UserContext);
-  const [loading, setloading] = useState(true);
+// import { useNavigate } from "react-router-dom";
+// import { UserContext } from "../context/user.context";
+// import { useContext, useEffect, useState } from "react";
+// const Userauth = ({ children }) => {
+//   const navigate = useNavigate();
+//   const { user } = useContext(UserContext);
+//   const [loading, setloading] = useState(true);
 
-  useEffect(() => {
-    if (user) {
-      return setloading(false);
-    }
-  }, []);
+//   useEffect(() => {
+//     if (user) {
+//       return setloading(false);
+//     }
+//   }, []);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-  return <>{children}</>;
-};
+//   if (loading) {
+//     return <div>Loading...</div>;
+//   }
+//   return <>{children}</>;
+// };
 
-export default Userauth;
+// export default Userauth;
